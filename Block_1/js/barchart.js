0f1d0c874f841d33f631ed228ef66c4073e250a2
illonion <zhou.lawrence.yz@gmail.com>
2021-03-29 15:47:26 +1100

    Fix Bar Graph Block 1

--- Block_1/js/barchart.js
+++ Block_1/js/barchart.js
@@ -1,33 +1,20 @@
-const width = 600
-const height = 400;
-const barPadding = 3;
+const width = 500;
+const height = 100;
+const barPadding = 2;
 
-let dataset = [14, 5, 26, 23, 9, 2, 7, 100, 24, 56, 45, 20];
-let svg = d3.select("body")
+const dataset = [14, 5, 26, 23, 9, 4, 20, 1, 8, 22];
+
+var svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-svg.selectAll("rect")
+    svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", function(d,i) {
-        return i * (width / dataset.length);
-    })
-    .attr("y", 0)
-    .attr("width", (width / dataset.length) - barPadding)
-    .attr("height", function(d, i) {
-        return dataset.TestData * 4;
-    });
-
-
-const bars = document.getElementsByTagName("rect");
-const noOfBars = bars.length;
-console.log(noOfBars);
-
-for (var i = 0; i < noOfBars; i++) {
-    if (bars[i].getAttribute("height") > 20) {
-        bars[i].style.backgroundColor = "red";
-    }
-}
+    .attr("x", function(d, i) { return i * (width / dataset.length); })
+    .attr("y", function(d) { return height - (d * 4); })
+    .attr("width", width / dataset.length - barPadding)
+    .attr("height", function(d) { return d * 4; })
+    .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
