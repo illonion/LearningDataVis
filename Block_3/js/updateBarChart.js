b91b1dd73cbdfa8dde194a05cae4dcc91d593e37
illonion <zhou.lawrence.yz@gmail.com>
2021-04-15 12:45:11 +1000

    Complete 3.1

--- Block_3/js/updateBarChart.js
+++ Block_3/js/updateBarChart.js
@@ -1,20 +1,48 @@
-const width = 500;
-const height = 100;
-const barPadding = 2;
+const width = 600;
+const height = 250;
+const maxValue = 25;
 
-const dataset = [14, 5, 26, 23, 9, 4, 20, 1, 8, 22];
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
+
+const xScale = d3.scaleBand()
+    .domain(d3.range(dataset.length))
+    .rangeRound([0, width])
+    .paddingInner(0.05);
+
+const yScale = d3.scaleLinear()
+    .domain([0,d3.max(dataset)])
+    .rangeRound([0, height])
 
 var svg = d3.select("body")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-    svg.selectAll("rect")
+svg.selectAll("rect")
     .data(dataset)
     .enter()
     .append("rect")
-    .attr("x", function(d, i) { return i * (width / dataset.length); })
-    .attr("y", function(d) { return height - (d * 4); })
-    .attr("width", width / dataset.length - barPadding)
-    .attr("height", function(d) { return d * 4; })
+    .attr("x", function(d, i) {return xScale(i);})
+    .attr("y", function(d) { return height - yScale(d); })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d) { return yScale(d); })
     .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
+
+d3.select("button")
+    .on("click", function() {
+        const numValues = dataset.length;
+        dataset = [];
+        for (var i = 0; i < numValues; i++)
+        {
+            dataset.push(Math.floor(Math.random() * maxValue));
+        }
+        
+        svg.selectAll("rect")
+            .data(dataset)
+            .attr("y", function(d) {
+                return height - yScale(d);
+            })
+            .attr("height", function(d) {
+                return yScale(d);
+            })
+    })
