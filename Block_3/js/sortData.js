48b42720ed3215e01392199e85423199c354c0f9
illonion <zhou.lawrence.yz@gmail.com>
2021-04-15 15:15:13 +1000

    Complete Block 3.5

---
+++ Block_3/js/sortData.js
@@ -0,0 +1,134 @@
+const width = 700;
+const height = 250;
+const maxValue = 25;
+
+let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
+
+const xScale = d3.scaleBand()
+    .domain(d3.range(dataset.length))
+    .rangeRound([0, width])
+    .paddingInner(0.05);
+
+const yScale = d3.scaleLinear()
+    .domain([0,d3.max(dataset)])
+    .rangeRound([0, height])
+
+let svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i) {return xScale(i);})
+    .attr("y", function(d) { return height - yScale(d); })
+    .attr("width", xScale.bandwidth())
+    .attr("height", function(d) { return yScale(d); })
+    .attr("fill", "grey")
+    .on("mouseover", function(d) {
+        var xPosition = parseFloat(d3.select(this).attr("x"));
+        var yPosition = parseFloat(d3.select(this).attr("y"));
+
+        d3.select(this)
+            .attr("fill", "orange");
+        
+        var tooltips = svg.append("text")
+            .attr("id", "tooltip")
+            .attr("x", xPosition + (xScale.bandwidth() / 4))
+            .attr("y", yPosition + 20)
+            .text(d)
+            .style("text-align", "center");
+            
+    })
+    .on("mouseout", function() {
+        d3.select(this)
+            .attr("fill", "grey");
+        
+        d3.select("#tooltip").remove();
+
+    });
+
+d3.select("#addData")
+    .on("click", function() {
+
+        dataset.push(Math.floor(Math.random() * maxValue));
+        xScale.domain(d3.range(dataset.length));
+        
+        const bars = svg.selectAll("rect")
+            .data(dataset);
+
+        bars.enter()
+            .append("rect")
+            .attr("x", width)
+            .attr("y", function(d) {return height - yScale(d);})
+            .merge(bars)
+            .transition()
+            .duration(500)
+            .attr("x", function(d, i) {return xScale(i);})
+            .attr("y", function(d) { return height - yScale(d); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) { return yScale(d); })
+            .attr("fill", "grey");
+
+        bars.on("mouseover", function(d) {
+            var xPosition = parseFloat(d3.select(this).attr("x"));
+            var yPosition = parseFloat(d3.select(this).attr("y"));
+    
+            d3.select(this)
+                .attr("fill", "orange");
+            
+            tooltips = svg.append("text")
+                .attr("id", "tooltip")
+                .attr("x", xPosition + (xScale.bandwidth() / 4))
+                .attr("y", yPosition + 20)
+                .text(d)
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .attr("fill", "grey");
+                
+                d3.select("#tooltip").remove();
+            });
+    })
+
+d3.select("#removeData")
+    .on("click", function() {
+
+        dataset.shift();
+
+        const bars = svg.selectAll("rect")
+            .data(dataset);
+
+        bars.exit()
+            .transition()
+            .duration(500)
+            .attr("x", width)
+            .remove();
+    })
+
+d3.select("#sortData")
+    .on("click", function() {
+        sortBars();
+    })
+
+var sortOrder = false;
+
+function sortBars() {
+
+    sortOrder = !sortOrder;
+    svg.selectAll("rect")
+        .sort(function(a, b) {
+            if (sortOrder) {
+                return d3.ascending(a,b);
+            } else {
+                return d3.descending(a,b);
+            }
+        })
+        .transition()
+        .duration(1500)
+        .attr("x", function(d, i) {
+            return xScale(i);
+        });
+}
