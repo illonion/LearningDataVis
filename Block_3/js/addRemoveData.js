bb6ebe206b6047962379e27265e34e32ba3a766f
illonion <zhou.lawrence.yz@gmail.com>
2021-04-15 13:56:21 +1000

    Complete Block 3.3

--- Block_3/js/addRemoveData.js
+++ Block_3/js/addRemoveData.js
@@ -2,7 +2,7 @@ const width = 600;
 const height = 250;
 const maxValue = 25;
 
-let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
+let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 5];
 
 const xScale = d3.scaleBand()
     .domain(d3.range(dataset.length))
@@ -28,77 +28,41 @@ svg.selectAll("rect")
     .attr("height", function(d) { return yScale(d); })
     .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
 
-d3.select("#updateData1")
+d3.select("#addData")
     .on("click", function() {
-        const numValues = dataset.length;
-        dataset = [];
-        for (let i = 0; i < numValues; i++)
-        {
-            dataset.push(Math.floor(Math.random() * maxValue));
-        }
+
+        dataset.push(Math.floor(Math.random() * maxValue));
+        xScale.domain(d3.range(dataset.length));
         
-        svg.selectAll("rect")
-            .data(dataset)
+        const bars = svg.selectAll("rect")
+            .data(dataset);
+
+        bars.enter()
+            .append("rect")
+            .attr("x", width)
+            .attr("y", function(d) {return height - yScale(d);})
+            .merge(bars)
             .transition()
-            .delay(function(d, i) {
-                return i / dataset.length * 1000;
-            })
             .duration(500)
-            .ease(d3.easeCircleOut)
-            .attr("y", function(d) {
-                return height - yScale(d);
-            })
-            .attr("height", function(d) {
-                return yScale(d);
-            })
+            .attr("x", function(d, i) {return xScale(i);})
+            .attr("y", function(d) { return height - yScale(d); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function(d) { return yScale(d); })
+            .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
     })
 
-d3.select("#updateData2")
+d3.select("#removeData")
     .on("click", function() {
-        const numValues = dataset.length;
-        dataset = [];
-        for (let i = 0; i < numValues; i++)
-        {
-            dataset.push(Math.floor(Math.random() * maxValue));
-        }
-        
-        svg.selectAll("rect")
-            .data(dataset)
-            .transition()
-            .delay(function(d, i) {
-                return i / dataset.length * 2000;
-            })
-            .duration(500)
-            .ease(d3.easeCircleIn)
-            .attr("y", function(d) {
-                return height - yScale(d);
-            })
-            .attr("height", function(d) {
-                return yScale(d);
-            })
-    })
 
-d3.select("#updateData3")
-    .on("click", function() {
-        const numValues = dataset.length;
-        dataset = [];
-        for (let i = 0; i < numValues; i++)
-        {
-            dataset.push(Math.floor(Math.random() * maxValue));
-        }
-        
-        svg.selectAll("rect")
-            .data(dataset)
+        dataset.shift();
+        xScale.domain(d3.range(dataset.length));
+
+        const bars = svg.selectAll("rect")
+            .data(dataset);
+
+        bars.exit()
             .transition()
-            .delay(function(d, i) {
-                return i / dataset.length * 2000;
-            })
             .duration(500)
-            .ease(d3.easeSinIn)
-            .attr("y", function(d) {
-                return height - yScale(d);
-            })
-            .attr("height", function(d) {
-                return yScale(d);
-            })
+            .attr("x", width)
+            .remove();
     })
