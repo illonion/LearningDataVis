19ca1505595b02736634a003c2e7ca48c72663b6
illonion <zhou.lawrence.yz@gmail.com>
2021-04-15 15:00:53 +1000

    Almost Block 3.4

--- Block_3/js/mouseover.js
+++ Block_3/js/mouseover.js
@@ -1,8 +1,8 @@
-const width = 600;
+const width = 700;
 const height = 250;
 const maxValue = 25;
 
-let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 5];
+let dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
 
 const xScale = d3.scaleBand()
     .domain(d3.range(dataset.length))
@@ -26,7 +26,29 @@ svg.selectAll("rect")
     .attr("y", function(d) { return height - yScale(d); })
     .attr("width", xScale.bandwidth())
     .attr("height", function(d) { return yScale(d); })
-    .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
+    .attr("fill", "grey")
+    .on("mouseover", function(d) {
+        var xPosition = parseFloat(d3.select(this).attr("x"));
+        var yPosition = parseFloat(d3.select(this).attr("y"));
+
+        d3.select(this)
+            .attr("fill", "orange");
+        
+        var tooltips = svg.append("text")
+            .attr("id", "tooltip")
+            .attr("x", xPosition + (xScale.bandwidth() / 4))
+            .attr("y", yPosition + 20)
+            .text(d)
+            .style("text-align", "center");
+            
+    })
+    .on("mouseout", function() {
+        d3.select(this)
+            .attr("fill", "grey");
+        
+        d3.select("#tooltip").remove();
+
+    });
 
 d3.select("#addData")
     .on("click", function() {
@@ -48,7 +70,27 @@ d3.select("#addData")
             .attr("y", function(d) { return height - yScale(d); })
             .attr("width", xScale.bandwidth())
             .attr("height", function(d) { return yScale(d); })
-            .attr("fill", function(d) { return "rgb(0, 0, " + Math.round(d * 10) + ")"; });
+            .attr("fill", "grey");
+
+        bars.on("mouseover", function(d) {
+            var xPosition = parseFloat(d3.select(this).attr("x"));
+            var yPosition = parseFloat(d3.select(this).attr("y"));
+    
+            d3.select(this)
+                .attr("fill", "orange");
+            
+            tooltips = svg.append("text")
+                .attr("id", "tooltip")
+                .attr("x", xPosition + (xScale.bandwidth() / 4))
+                .attr("y", yPosition + 20)
+                .text(d)
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .attr("fill", "grey");
+                
+                d3.select("#tooltip").remove();
+            });
     })
 
 d3.select("#removeData")
