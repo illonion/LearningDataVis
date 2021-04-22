a7f2c0db1be9be99037a474aa1d89330174ce6ab
illonion <zhou.lawrence.yz@gmail.com>
2021-04-22 16:45:10 +1000

    Complete Block 4.4

---
+++ Block_4/js/geoPathsProjections.js
@@ -0,0 +1,22 @@
+var w = 500;
+var h = 300;
+
+var projection = d3.geoMercator()
+    .center([145, -36.5])
+    .translate([w/2, h/2])
+    .scale(2450);
+var path = d3.geoPath().projection(projection);
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("fill", "grey");
+
+d3.json("resources/LGA_VIC.json").then(function(json) {
+    svg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path);
+});
